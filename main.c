7b42f4eb9ca0dc2efcc58a14cedaf76f67e38cd4
Alisson <alissonmarcos249@mail.com>
2024-01-03 14:24:51 -0300

    esse e o commit de alisson

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+
 int main(void)
 {
-	printf("hello melvin!");
+	int len;
+	char *str = "hello world\n";
+
+	len = ft_strlen(str);
+	printf("%d\n", len);
 	return (0);
 }
